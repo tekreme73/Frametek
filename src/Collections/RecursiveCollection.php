40e6c3503d52c9f531b2afb3f0d721272baa83f8
tekreme73 <tekreme73@gmail.com>
2015-06-03 10:09:11 +0200

    Added configuration support

--- src/Collections/RecursiveCollection.php
+++ src/Collections/RecursiveCollection.php
@@ -8,8 +8,6 @@
  */
 namespace Frametek\Collections;
 
-use Frametek\Interfaces\CollectionInterface;
-
 /**
  * RecursiveCollection
  *
---
+++ tests/Persistent/config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+		
+		'site'    => array(
+                
+				'name'      => 'BOB',
+				'age'       => 50,
+		),
+		
+		'mail'    => 'bob@test.com',
+);
